61a7414f3cfbc7cf0f591ea8b351514f1d409b84
alicio <alicio.fcn@gmail.com>
2022-06-30 17:04:16 -0300

    validacao no form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Task } from './model/task';
 
 @Component({
@@ -9,10 +10,34 @@ import { Task } from './model/task';
 export class AppComponent {
   public title: String = 'Lista de tarefas';
   public listaTarefas: Task[] = [];
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      decricao: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(50),
+        Validators.required
+      ])]
+    });
+
+
     this.listaTarefas.push(new Task(1, 'Ir ao mercado', false));
     this.listaTarefas.push(new Task(2, 'Cortar o cabelo', false));
     this.listaTarefas.push(new Task(3, 'Ir ao posto', false));
+  }
+
+
+  remove(item: Task) {
+    
+  }
+
+  markAsDone(item: Task) {
+    
+  }
+
+  markAsUndone(item: Task) {
+    
   }
 }
